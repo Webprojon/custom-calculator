6d38c99a4a80ff3be517f2c826809d0e715a9f96
webprojon <tokhirjon.khasanov@gmail.com>
2025-09-06 20:51:12 +0200

    chore: setup project with Webpack, ESLint, Prettier, Husky, Jest, and base folder structure

---
+++ src/tests/add.test.js
@@ -0,0 +1,5 @@
+describe('AddCommand', () => {
+  test('placeholder test', () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ src/tests/factorial.test.js
@@ -0,0 +1,5 @@
+describe('FactorialCommand', () => {
+  test('placeholder test', () => {
+    expect(true).toBe(true);
+  });
+});
